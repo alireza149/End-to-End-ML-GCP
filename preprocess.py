2c1d3bf87ddbff79634bd0321d0e13e0d9900b4d
Alireza <alireza0494@gmail.com>
2020-01-15 14:31:31 +0000

    commit_2

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,20 @@ class MySimpleScaler(object):
  
 
  def preprocess(self, data):
+    
+    data = data[data.weight_pounds > 0]
+    data = data[data.mother_age  > 0]
+    data = data[data.plurality > 0]
+    data = data[data.gestation_weeks > 0]
+    print(data.shape)
+
+    
     # Get one hot encoding of columns B
     one_hot = pd.get_dummies(data['is_male'])
     # Drop column B as it is now encoded
     data = data.drop('is_male',axis = 1)
     # Join the encoded df
     data = data.join(one_hot)
+
 
     return data
