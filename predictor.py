358e22a6e25ee0571f1f113b26279c9b70d25639
alireza <alireza0494@gmail.com>
2020-01-16 11:42:30 +0000

    commit 4 - 16/01

---
+++ predictor.py
@@ -0,0 +1,26 @@
+import os
+import pickle
+
+import numpy as np
+from sklearn.externals import joblib
+
+class MyPredictor(object):
+  def __init__(self, model, preprocessor):
+    self._model = model
+    self._preprocessor = preprocessor
+    #self._class_names = load_iris().target_names
+
+  def predict(self, instances, **kwargs):
+    inputs = np.asarray(instances)
+    preprocessed_inputs = self._preprocessor.preprocess(inputs)
+
+  @classmethod
+  def from_path(cls, model_dir):
+    model_path = os.path.join(model_dir, 'model.joblib')
+    model = joblib.load(model_path)
+
+    preprocessor_path = os.path.join(model_dir, 'preprocessor.pkl')
+    with open(preprocessor_path, 'rb') as f:
+      preprocessor = pickle.load(f)
+
+    return cls(model, preprocessor)
